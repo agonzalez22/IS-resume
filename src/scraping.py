89e02a5ec9a977b9b86754be9182a12aaac32e61
Alina <55455721+agonzalez22@users.noreply.github.com>
2024-05-03 13:40:21 -0400

    added commit api

--- src/scraping.py
+++ src/scraping.py
@@ -3,22 +3,66 @@ Contains all the functions for scraping
 """ 
 import requests
 from collections import defaultdict
+import time 
 
 def get_repos(username): 
     """ given a valid git username, get all the repos in a list 
     Args: 
         username (str): username of github user
     Returns: 
-        repos (list): list of repos as strings
+        repos (dict): dict of repos with the owner as the value
     """
     url = f'https://api.github.com/users/{username}/repos'
     data = requests.get(url).json()
     
     # automatically skips over private repos
-    repos = [repo['name'] for repo in data]
-  
+    repos = {repo['name']: repo['owner']['login'] for repo in data}
+
     return repos
 
+def get_repo_shas(owner, repo, user): 
+    """ gets the SHAs from a given repo and stores in a list. 
+    get_repo_shas
+    """
+    time.sleep(1)
+    url = f'https://api.github.com/repos/{owner}/{repo}/commits'
+    commits_log = requests.get(url).json()
+
+    shas = []
+    for commit in commits_log: 
+        try: 
+            if commit['author']['login'] == user: # cross checks against our user
+                shas.append(commit['sha']) # appends if correct user
+        except: 
+            pass # am getting a nonetype error here, if someone could look at it that would be great
+    return shas
+
+
+def get_commit_files(owner, repo, user): 
+    shas = get_repo_shas(owner, repo, user)
+
+    file_types = defaultdict(int)
+    for sha in shas: 
+        time.sleep(1)
+        url = f'https://api.github.com/repos/{owner}/{repo}/commits/{sha}'
+        data = requests.get(url).json()
+        
+        for file in data['files']: 
+            end = file['filename'].split('.')[-1].lower() # gets the end of a file 
+            file_types[end] += file['additions'] + file['deletions'] # gets the bytes adjusted 
+    
+    return file_types
+
+# test for getting stats thru commits...
+test = get_repos('agonzalez22')
+for key, value in test.items(): 
+    get_commit_files(value, key, 'agonzalez22')
+    break 
+
+
+""" GETTING GITHUB STATS THRU REPO STATS """
+
+    
 def get_lang_stats(username, repo): 
     """ given a user and a repo, get all the lang stats 
     Args: 
@@ -59,5 +103,6 @@ def get_total_stats(username):
     return stats
 
 # example (don't have a main func for utils please)
-alex_stats = get_total_stats('kloafe')
+# alex_stats = get_total_stats('Dao-Ho')
+
 
