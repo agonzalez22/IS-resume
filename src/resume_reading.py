603f670d195b974c9019617efa21bd2a254659a5
alex_chen_04 <127902759+Kloafe@users.noreply.github.com>
2024-05-04 15:23:25 -0400

    some changes

---
+++ src/resume_reading.py
@@ -0,0 +1,17 @@
+import pdfreader
+from pdfreader import PDFDocument, SimplePDFViewer
+import os
+
+def get_file_paths(directory):
+    """ get files paths
+    args:
+    directory: directory where resumes are stored
+
+    return:
+    resumes: (lst) list of directory names
+    
+    """
+
+
+
+def read_pdf(pdf)
